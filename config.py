9a67d5b08b43f7598da273b513e5e9bdba4b9b05
Private-Repo-RP <r91834675@gmail.com>
2026-07-03 18:27:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Private-Repo-RP/Private-Repo-RP",
+    "https://github.com/Private-Repo-RP/Ora",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None) or getenv("GITHUB_TOKEN", None)
