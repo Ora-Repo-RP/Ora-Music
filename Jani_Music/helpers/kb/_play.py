a2fbf71fd775d9bb4be86b37314c5d957724eb30
Ora-Repo-RP <ramrp1460@gmail.com>
2026-07-09 15:06:52 +0530

    Update _play.py

--- Jani_Music/helpers/kb/_play.py
+++ Jani_Music/helpers/kb/_play.py
@@ -34,28 +34,28 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100 if duration_sec else 0
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "✄·─·─·─·─·─·─·─·─·─"
+        bar = "≽╾╾╾╾╾╾╾╾╾╾╾╾"
     elif 10 < umm < 20:
-        bar = "-ˋˏ✄·─·─·─·─·─·─·─·─"
+        bar = "╾≽╾╾╾╾╾╾╾╾╾╾╾"
     elif 20 <= umm < 30:
-        bar = "-ˋˏ-ˋˏ✄·─·─·─·─·─·─·─"
+        bar = "╾╾≽╾╾╾╾╾╾╾╾╾╾"
     elif 30 <= umm < 40:
-        bar = "-ˋˏ-ˋˏ-ˋˏ✄·─·─·─·─·─·─"
+        bar = "╾╾╾≽╾╾╾╾╾╾╾╾╾"
     elif 40 <= umm < 50:
-        bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─·─·─·─·─"
+        bar = "╾╾╾╾╾≽╾╾╾╾╾╾╾"
     elif 50 <= umm < 60:
-        bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─·─·─·─"
+        bar = "╾╾╾╾╾╾╾≽╾╾╾╾╾"
     elif 60 <= umm < 70:
-        bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─·─·─"
+        bar = "╾╾╾╾╾╾╾╾╾≽╾╾╾"
     elif 70 <= umm < 80:
-        bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─·─"
+        bar = "╾╾╾╾╾╾╾╾╾╾╾≽╾"
     elif 80 <= umm < 95:
-        bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─"
+        bar = "╾╾╾╾╾╾╾╾╾╾╾╾≽"
     else:
-        bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·"
+        bar = "╾ᴺᵉˣᵗ ˢᵒⁿᵍ ᴾˡᵃʸ ᴺᵒʷ╾"
     buttons = [
         [
-            success_button(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
+            success_button(text="▉▉", callback_data=f"ADMIN Pause|{chat_id}"),
             success_button(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             primary_button(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             success_button(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
@@ -74,7 +74,7 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            success_button(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
+            success_button(text="▉▉", callback_data=f"ADMIN Pause|{chat_id}"),
             success_button(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             primary_button(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             success_button(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
