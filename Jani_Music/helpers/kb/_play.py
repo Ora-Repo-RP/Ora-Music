bae173e45c2caf6b45c6471ae460e3d3d307ca39
Ora-Repo-RP <ramrp1460@gmail.com>
2026-07-09 14:18:21 +0530

    Update _play.py

--- Jani_Music/helpers/kb/_play.py
+++ Jani_Music/helpers/kb/_play.py
@@ -34,25 +34,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100 if duration_sec else 0
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "✄·─·─·─·─·─·─·─·─·─"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "-ˋˏ✄·─·─·─·─·─·─·─·─"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "-ˋˏ-ˋˏ✄·─·─·─·─·─·─·─"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "-ˋˏ-ˋˏ-ˋˏ✄·─·─·─·─·─·─"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─·─·─·─·─"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─·─·─·─"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─·─·─"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─·─"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─"
     else:
-        bar = "—————————◉"
+        bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·"
     buttons = [
         [
             success_button(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
@@ -67,6 +67,7 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
+        [danger_button(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true")],
         [danger_button(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
@@ -80,6 +81,7 @@ def stream_markup(_, chat_id):
             success_button(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             success_button(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
+        [danger_button(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true")],
         [danger_button(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
